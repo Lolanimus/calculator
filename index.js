9c7004a0d9eaaeda90249ea5e8e9d8b57c2e6cd6
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2023-07-08 13:18:58 +0800

    Made fundamental calculator

---
+++ index.js
@@ -0,0 +1,95 @@
+let buttons = document.querySelectorAll('button');
+let resultScreen = document.querySelector('.result');
+let firstNumber;
+let operator;
+let secondNumber;
+let result;
+
+function add(a, b) {
+    resultScreen.textContent = a + b; 
+}
+
+function subtract(a, b) {
+    resultScreen.textContent = a - b; 
+}
+
+function multiply(a, b) {
+    resultScreen.textContent = a * b; 
+}
+
+function divide(a, b) {
+    resultScreen.textContent = a / b; 
+}
+
+function fromStringtoNumA(a) {
+    // if(a.includes('.')) {
+    //     return parseFloat(a);
+    // } else {
+        return parseInt(a);
+    // }
+}
+
+function fromStringtoNumB(b) {
+    // if(b.includes('.')) {
+    //     return parseFloat(b);
+    // } else {
+        return parseInt(b);
+    // }
+}
+
+function operatorDeterminizer(a, oper, b) {
+    if(oper == '+') add(a, b);
+    if(oper == '-') subtract(a, b);
+    if(oper == '*') multiply(a, b);
+    if(oper == '/') divide(a, b);
+}
+
+function doTheEquation(a, oper, b) {
+    a = fromStringtoNumA(a);
+    b = fromStringtoNumB(b);
+    operatorDeterminizer(a, oper, b);
+}
+
+function pushNumber(element) {
+    resultScreen.textContent += element.id;
+}
+
+function pushOperator(element) {
+    resultScreen.textContent += element.textContent;
+    buttons.forEach(item => {
+        if(item.classList.contains("oper")) {
+            item.disabled = true;
+        };
+    })
+}
+
+function pushClear() {
+    resultScreen.textContent = "";
+    buttons.forEach(item => {
+        if(item.classList.contains("oper")) {
+            item.disabled = false;
+        };
+    })
+}
+
+let counter1 = 0;
+let counter2 = 0;
+
+function pushEqual() {
+    let resultArr = resultScreen.textContent.split('');
+    resultArr.forEach(item => {
+        if(isNaN(item)) {
+            counter1++;
+            return item;
+        }
+
+        if(counter1 === 0) {
+            counter2++;
+            return item;
+        }
+    })
+    firstNumber = resultArr.splice(0, counter2).join('');
+    operator = resultArr.shift();
+    secondNumber = resultArr.join('');
+    doTheEquation(firstNumber, operator, secondNumber);
+}
